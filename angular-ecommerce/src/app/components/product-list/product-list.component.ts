0f3816afb74bb99e509a4b2fe13bdd0c4dabc03f
Fazal Haroon <kharoon187@gmail.com>
2023-02-18 16:31:46 +0400

    Angular - get product list service

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+    products: Product[] = [];
+
+    constructor(private productService: ProductService) {
+    }
+
+    ngOnInit(): void { //Similar to @PostConstruct
+        this.listProducts();
+    }
+
+    listProducts() {
+        this.productService.getProductList().subscribe(
+            data => {
+                this.products = data;
+            }
+        )
+    }
+
 
 }
