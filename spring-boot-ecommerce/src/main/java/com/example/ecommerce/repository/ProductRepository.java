ef249756dd5eb93b0e533d749b3e2782136704c6
Fazal Haroon <kharoon187@gmail.com>
2023-02-18 16:08:10 +0400

    Springboot - Cross Origin added 2

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-// @CrossOrigin({"http://localhost:4200", "http://localhost:3000"})  // for multiple
-// @CrossOrigin  // wildcard (any website)
+//@CrossOrigin({"http://localhost:4200", "http://localhost:3000"})  // for multiple
+//@CrossOrigin  // wildcard (any website)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
