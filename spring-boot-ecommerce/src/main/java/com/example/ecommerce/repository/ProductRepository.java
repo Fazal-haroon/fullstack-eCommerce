b82d0a8615cbe0b5cbcbe0e2c6e540f24e487bba
Fazal Haroon <kharoon187@gmail.com>
2023-02-21 14:57:52 +0400

    Springboot - Spring data Rest Repository new method findByCategoryId added

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
@@ -9,4 +12,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 //@CrossOrigin({"http://localhost:4200", "http://localhost:3000"})  // for multiple
 //@CrossOrigin  // wildcard (any website)
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
