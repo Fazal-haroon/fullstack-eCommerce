aca10b27296d37e2a112ac17d4be23d6587b5d47
Fazal Haroon <kharoon187@gmail.com>
2023-02-22 18:02:49 +0400

    Angular - Update thePageSize

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -17,7 +17,7 @@ export class ProductListComponent implements OnInit {
 
     //New properties for pagination
     thePageNumber: number = 1;
-    thePageSize: number = 10;
+    thePageSize: number = 5;
     theTotalElements: number = 0;
 
     constructor(private productService: ProductService, private route: ActivatedRoute) {
