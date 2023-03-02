02c7168435419360609c024490d8371197b2632e
Fazal Haroon <kharoon187@gmail.com>
2023-03-02 23:27:34 +0400

    Angular - Checkout - Review Your Order

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit{
 
   checkoutFormGroup: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   billingAddressStates: any;
 
   shippingAddressStates: any;
