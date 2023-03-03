dcf52fa3f741e7f3726f5163e7e823fc0c8ae992
Fazal Haroon <kharoon187@gmail.com>
2023-03-03 19:55:11 +0400

    Springboot - create Repository for Country and state

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/entity/Country.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,6 @@ public class Country {
 
     // Set up one-to-many with states
     @OneToMany(mappedBy = "country") //One country has many states
+    @JsonIgnore //will ignore the states
     private List<State> states;
 }
