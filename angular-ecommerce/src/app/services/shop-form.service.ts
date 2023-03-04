66406338f1c0e7901c1a17e43a4bb8ab9ad56553
Fazal Haroon <kharoon187@gmail.com>
2023-03-04 16:57:10 +0400

    Angular - Checkout Form - create TypeScript classes for(Country & state) and add methods to the form service for(countries & states)

--- angular-ecommerce/src/app/services/shop-form.service.ts
+++ angular-ecommerce/src/app/services/shop-form.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs"; // rxjs: Reactive JavaScript
+import {map, Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {State} from "../common/state"; // rxjs: Reactive JavaScript
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
@@ -33,4 +39,28 @@ export class ShopFormService {
     return of(data);
   }
 
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+        map(response => response._embedded.countries)
+    )
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    return this.httpClient.get<GetResponseStates>(`${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`).pipe(
+        map(response => response._embedded.states)
+    )
+  }
+
+}
+
+interface GetResponseCountries{
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates{
+  _embedded: {
+    states: State[];
+  }
 }
