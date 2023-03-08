1a3228818496e3cb153d04a42565a883379605bf
Fazal Haroon <kharoon187@gmail.com>
2023-03-08 16:13:12 +0400

    Springboot - Run Database Script for address, customer, orders and order_item tables

--- spring-boot-ecommerce/src/main/resources/scripts/DDL/script-v0.1.sql
+++ spring-boot-ecommerce/src/main/resources/scripts/DDL/script-v0.1.sql
@@ -67,3 +67,70 @@ CREATE TABLE `state` (
                          KEY `fk_country` (`country_id`),
                          CONSTRAINT `fk_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
+-- -----------------------------------------------------
+-- Table `full-stack-ecommerce`.`address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address`;
+
+CREATE TABLE `address` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `city` varchar(255) DEFAULT NULL,
+                           `country` varchar(255) DEFAULT NULL,
+                           `state` varchar(255) DEFAULT NULL,
+                           `street` varchar(255) DEFAULT NULL,
+                           `zip_code` varchar(255) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- -----------------------------------------------------
+-- Table `full-stack-ecommerce`.`customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+                            `id` bigint NOT NULL AUTO_INCREMENT,
+                            `first_name` varchar(255) DEFAULT NULL,
+                            `last_name` varchar(255) DEFAULT NULL,
+                            `email` varchar(255) DEFAULT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- -----------------------------------------------------
+-- Table `full-stack-ecommerce`.`orders`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+                          `id` bigint NOT NULL AUTO_INCREMENT,
+                          `order_tracking_number` varchar(255) DEFAULT NULL,
+                          `total_price` decimal(19,2) DEFAULT NULL,
+                          `total_quantity` int DEFAULT NULL,
+                          `billing_address_id` bigint DEFAULT NULL,
+                          `customer_id` bigint DEFAULT NULL,
+                          `shipping_address_id` bigint DEFAULT NULL,
+                          `status` varchar(128) DEFAULT NULL,
+                          `date_created` datetime(6) DEFAULT NULL,
+                          `last_updated` datetime(6) DEFAULT NULL,
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `UK_billing_address_id` (`billing_address_id`),
+                          UNIQUE KEY `UK_shipping_address_id` (`shipping_address_id`),
+                          KEY `K_customer_id` (`customer_id`),
+                          CONSTRAINT `FK_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+                          CONSTRAINT `FK_billing_address_id` FOREIGN KEY (`billing_address_id`) REFERENCES `address` (`id`),
+                          CONSTRAINT `FK_shipping_address_id` FOREIGN KEY (`shipping_address_id`) REFERENCES `address` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- -----------------------------------------------------
+-- Table `full-stack-ecommerce`.`order_item`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `order_item`;
+
+CREATE TABLE `order_item` (
+                              `id` bigint NOT NULL AUTO_INCREMENT,
+                              `image_url` varchar(255) DEFAULT NULL,
+                              `quantity` int DEFAULT NULL,
+                              `unit_price` decimal(19,2) DEFAULT NULL,
+                              `order_id` bigint DEFAULT NULL,
+                              `product_id` bigint DEFAULT NULL,
+                              PRIMARY KEY (`id`),
+                              KEY `K_order_id` (`order_id`),
+                              CONSTRAINT `FK_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+                              CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
