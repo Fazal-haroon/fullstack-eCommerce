4e660303b3483da4f3c365f8dfdbc641707975ea
Fazal Haroon <kharoon187@gmail.com>
2023-03-13 18:59:34 +0400

    Angular - Security - update app module configs to connect routes part 1

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -17,6 +17,13 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG} from "@okta/okta-angular";
+import {OktaAuth} from "@okta/okta-auth-js";
+import myAppConfig from "./config/my-app-config";
+
+const oktaConfig = myAppConfig.oidc;
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 @NgModule({
     declarations: [
