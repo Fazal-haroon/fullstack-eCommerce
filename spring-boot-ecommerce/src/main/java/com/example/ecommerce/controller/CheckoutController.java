31277134f4f0251f61076720187bfc60bd24f197
Fazal Haroon <kharoon187@gmail.com>
2023-03-17 23:51:42 +0400

    Springboot - Refactoring - configure CORS mapping for @RestController

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
@@ -6,7 +6,7 @@ import com.example.ecommerce.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin()
+//@CrossOrigin()
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
