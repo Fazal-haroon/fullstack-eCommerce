12df4cd382bcf0cb68512b55b0ed9dafdd6fb66c
Fazal Haroon <kharoon187@gmail.com>
2023-03-17 23:56:58 +0400

    Springboot - Refactoring - Disable HTTP PATCH method and modify spring data REST Detection Strategy

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/StateRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/StateRepository.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 //@CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "states", path = "states")
+@RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
 
     List<State> findByCountryCode(@Param("code") String code);
