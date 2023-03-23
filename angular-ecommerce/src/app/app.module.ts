d2f8a435328a55ae4cd2699c34e09307dec17e30
Fazal Haroon <kharoon187@gmail.com>
2023-03-23 14:37:21 +0400

    Angular - Security - Order history frontend - Update app.modules.ts to reference interceptor

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {ProductListComponent} from './components/product-list/product-list.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {AppRoutingModule} from './app-routing.module';
 import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
@@ -28,6 +28,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import {AuthInterceptorService} from "./services/auth-interceptor.service";
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -56,7 +57,8 @@ const oktaAuth = new OktaAuth(oktaConfig);
         ReactiveFormsModule,
         OktaAuthModule
     ],
-    providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth }}],
+    providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth }},
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
     bootstrap: [AppComponent]
 })
 export class AppModule {
