8c8c2fa1a8f38ddc1c7a2003158b5483d96c8846
Fazal Haroon <kharoon187@gmail.com>
2023-03-23 14:29:48 +0400

    Angular - Security - Order history frontend - Pass access token in interceptor

--- angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {from, Observable} from "rxjs";
+import {from, lastValueFrom, Observable} from "rxjs";
+import {OKTA_AUTH} from "@okta/okta-angular";
+import {OktaAuth} from "@okta/okta-auth-js";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return from(this.handleAccess(request, next));
   }
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    
+
+    //Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8082/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      //get access token
+      const accessToken = this.oktaAuth.getAccessToken();
+
+      //clone the request and add new header with access token
+      request = request.clone({
+        setHeaders:{
+          Authorization: 'Bearer ' + accessToken //we clone b/c request is immutable
+        }
+      });
+    }
+    return await lastValueFrom(next.handle(request));
   }
 }
