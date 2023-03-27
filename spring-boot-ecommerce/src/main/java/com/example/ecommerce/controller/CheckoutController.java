dfc5587d55a195bbd870c09a0a5d5b9de4a88f84
Fazal Haroon <kharoon187@gmail.com>
2023-03-27 17:19:00 +0400

    springboot - changes in controller

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.dto.PaymentInfo;
 import com.example.ecommerce.dto.Purchase;
 import com.example.ecommerce.dto.PurchaseResponse;
 import com.example.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin()
@@ -11,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
-    private CheckoutService checkoutService;
+    private final CheckoutService checkoutService;
 
     @Autowired
     public CheckoutController(CheckoutService checkoutService) {
@@ -20,8 +25,7 @@ public class CheckoutController {
 
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
-        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
-        return purchaseResponse;
+        return checkoutService.placeOrder(purchase);
     }
 
     /*
@@ -30,4 +34,11 @@ public class CheckoutController {
     * Since we are not using Cookies for session tracking, CSRF says request is unauthorized
     * We can resolve this by disabling CSRF, This technique is commonly used for REST APIs
     * */
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
 }
