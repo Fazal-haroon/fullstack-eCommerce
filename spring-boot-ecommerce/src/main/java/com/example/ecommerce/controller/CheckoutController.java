001114d0aafbcbba99cfca9f71163488140c3a91
Fazal Haroon <kharoon187@gmail.com>
2023-03-27 15:50:10 +0400

    Springboot - credit card payment processing with Stripe - added PaymentIntent in Checkout Controller

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.dto.PaymentInfo;
 import com.example.ecommerce.dto.Purchase;
 import com.example.ecommerce.dto.PurchaseResponse;
 import com.example.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin()
@@ -30,4 +35,11 @@ public class CheckoutController {
     * Since we are not using Cookies for session tracking, CSRF says request is unauthorized
     * We can resolve this by disabling CSRF, This technique is commonly used for REST APIs
     * */
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
 }
